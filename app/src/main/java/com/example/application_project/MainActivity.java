d39bd3f3293a28aaebbd47cb6692d7f3d23063aa
KevinLai02 <jihn.smith629@gmail.com>
2025-01-05 23:55:49 +0800

    accounting feature

--- app/src/main/java/com/example/application_project/MainActivity.java
+++ app/src/main/java/com/example/application_project/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.application_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void goto_accounting(View view) {
+        Intent intent = new Intent(this, AccountingActivity.class);
+        startActivity(intent);
     }
 }
